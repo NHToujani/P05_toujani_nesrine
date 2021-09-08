a9880affbdbd658f80aab6f332f24144eb029a90
Nesrine <nesrine.hamriteptoujani@gmail.com>
2021-09-08 23:37:52 +0200

    change XMLHttpRequest by fetch method

--- frontend/product.js
+++ frontend/product.js
@@ -15,101 +15,112 @@ function getUrlParametre(sVar) {
 /* Récupérer les détails d'un produit */
 /* paramètre d'entrée: id produit*/
 /* paramètre sortie: détail produit: caméras*/
-async function getDetailProduct() {
-  let idProduct = getUrlParametre("id");
-  const requestURL = "http://localhost:3000/api/cameras";
-  let request = new XMLHttpRequest();
-  request.open("GET", requestURL);
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    let cameras = request.response;
+ 
+// /* Obtenir l'ID du produit de l'URL*/
+// function getProductId() {
+//   return new URL(document.location).searchParams.get('id');
+// };
+// /* Appeler les données par ID de l'API du serveur  */
+// function getProductInfo(productId) {
+//   return fetch(`http://localhost:3000/api/cameras/${productId}`)
+//     .then((responseHttp) => responseHttp.json())
+//     .catch(function (error) {
+//       showErrorMessage()
+//     });
+// }; ==> à vérifier avec Omar
+let idProduct = getUrlParametre("id");
+const requestURL = `http://localhost:3000/api/cameras/${idProduct}`;
+fetch(requestURL, {
+  method: 'GET'
+})
+  .then((response) => response.json())
+  .then((data) => {
 
+    let cameras = data;
     console.log(cameras);
-    showCameras(cameras);
-  };
+    showCameras(cameras)
+  })
+  .catch((error) => {
+    alert(`Il y a eu une erreur : ` + error)
+  });
 
-  function showCameras(jsonObj) {
-    const elt = document.getElementById("product");
-    let listCameras = jsonObj;
-    let camera = listCameras.find((x) => x._id === idProduct);
-    console.log(camera);
+function showCameras(camera) {
+  const elt = document.getElementById("product");
 
-    window.localStorage.setItem("productdetails", JSON.stringify(camera));
+  window.localStorage.setItem("productdetails", JSON.stringify(camera));
 
-    const ajouterPanier = "Ajouter au panier";
+  const ajouterPanier = "Ajouter au panier";
 
-    let myContainer = document.createElement("article");
-    myContainer.setAttribute("class", "row col-12");
+  let myContainer = document.createElement("article");
+  myContainer.setAttribute("class", "row col-12");
 
-    let myImageContainer = document.createElement("div");
-    myImageContainer.setAttribute("class", "cover col-lg-6");
-    let myCamera = document.createElement("img");
-    myCamera.classList.add("w-100");
+  let myImageContainer = document.createElement("div");
+  myImageContainer.setAttribute("class", "cover col-lg-6");
+  let myCamera = document.createElement("img");
+  myCamera.classList.add("w-100");
 
-    let myDetailContainer = document.createElement("div");
-    myDetailContainer.setAttribute("class", "col-lg-6");
-    let myTitle = document.createElement("h5");
-    myTitle.classList.add("card-title");
-    let myDescription = document.createElement("p");
-    myDescription.classList.add("card-text");
-    let myPrice = document.createElement("p");
-    myPrice.classList.add("card-price");
-    let myLensesContainer = document.createElement("p");
-    let myLenses = document.createElement("select");
-    myLenses.setAttribute("id", "selectlense");
-    myLenses.setAttribute("class", "form-select lenses");
-    myLenses.setAttribute("aria-label", "Default select");
-    let listlenses = camera.lenses;
-    for (let i = 0; i < listlenses.length; i++) {
-      if (i == 0) {
-        let firstlense = document.createElement("option");
-        firstlense.setAttribute("selected", "selected");
-        firstlense.textContent = "Open this select menu";
-        myLenses.appendChild(firstlense);
-      };
-
-      let lense = document.createElement("option");
-      lense.textContent = listlenses[i];
-      console.log(lense);
-      myLenses.appendChild(lense);
+  let myDetailContainer = document.createElement("div");
+  myDetailContainer.setAttribute("class", "col-lg-6");
+  let myTitle = document.createElement("h5");
+  myTitle.classList.add("card-title");
+  let myDescription = document.createElement("p");
+  myDescription.classList.add("card-text");
+  let myPrice = document.createElement("p");
+  myPrice.classList.add("card-price");
+  let myLensesContainer = document.createElement("p");
+  let myLenses = document.createElement("select");
+  myLenses.setAttribute("id", "selectlense");
+  myLenses.setAttribute("class", "form-select lenses");
+  myLenses.setAttribute("aria-label", "Default select");
+  let listlenses = camera.lenses;
+  for (let i = 0; i < listlenses.length; i++) {
+    if (i == 0) {
+      let firstlense = document.createElement("option");
+      firstlense.setAttribute("selected", "selected");
+      firstlense.textContent = "Choose lenses";
+      myLenses.appendChild(firstlense);
     };
-    myLensesContainer.appendChild(myLenses);
 
-    let myButton = document.createElement("button");
-    myButton.setAttribute("class", "btn ajouterPanier");
-    myButton.setAttribute("onclick", "addtopanel()");
-
-    myCamera.setAttribute("src", camera.imageUrl);
-    myCamera.setAttribute("id", camera._id);
-    myTitle.textContent = camera.name;
-    myDescription.textContent = camera.description;
-    myPrice.textContent = camera.price.toLocaleString("fr-FR", {
-      style: "currency",
-      currency: "EUR",
-    });
-    myButton.textContent = ajouterPanier;
-
-    myImageContainer.appendChild(myCamera);
-
-    myDetailContainer.appendChild(myTitle);
-    myDetailContainer.appendChild(myDescription);
-    myDetailContainer.appendChild(myPrice);
-
-    myDetailContainer.appendChild(myLensesContainer);
-    myDetailContainer.appendChild(myButton);
-
-    myContainer.appendChild(myImageContainer);
-    myContainer.appendChild(myDetailContainer);
-
-    elt.appendChild(myContainer);
+    let lense = document.createElement("option");
+    lense.textContent = listlenses[i];
+    console.log(lense);
+    myLenses.appendChild(lense);
   };
-}
+  myLensesContainer.appendChild(myLenses);
+
+  let myButton = document.createElement("button");
+  myButton.setAttribute("class", "btn ajouterPanier");
+  myButton.setAttribute("onclick", "addtopanel()");
+
+  myCamera.setAttribute("src", camera.imageUrl);
+  myCamera.setAttribute("id", camera._id);
+  myTitle.textContent = camera.name;
+  myDescription.textContent = camera.description;
+  myPrice.textContent = camera.price.toLocaleString("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+  });
+  myButton.textContent = ajouterPanier;
+
+  myImageContainer.appendChild(myCamera);
+
+  myDetailContainer.appendChild(myTitle);
+  myDetailContainer.appendChild(myDescription);
+  myDetailContainer.appendChild(myPrice);
+
+  myDetailContainer.appendChild(myLensesContainer);
+  myDetailContainer.appendChild(myButton);
+
+  myContainer.appendChild(myImageContainer);
+  myContainer.appendChild(myDetailContainer);
+
+  elt.appendChild(myContainer);
+};
+
 
 getDetailProduct();
 
 function addlenses() {
-  console.log("here i am ");
   let product = JSON.parse(window.localStorage.getItem("productdetails"));
   product.lenses = [];
   let selectedlenses = document.getElementById("selectlense");
